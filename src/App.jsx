e706748357f811e036727f0509afa51ca6bd3245
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-16 14:20:50 -0500

    search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonName, setPokemonName] = useState("")
+
+  const searchPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+      console.log(response)
+    })
+  }
 
   return (
     <div className="App">
       <div className='TitleSection'>
 <h1>Pokemon</h1>
-<input type="text" />
-<button>Search Pokemon</button>
+<input type="text" onChange={(event) => {setPokemonName(event.target.value)}} />
+<button onClick={searchPokemon}>Search Pokemon</button>
   </div>
     </div>
   )
